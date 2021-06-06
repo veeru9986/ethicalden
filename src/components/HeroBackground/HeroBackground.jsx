f019d24b04da90f8841fda1a416149b3d3382404
Veeresh603 <analogica@analogica.in>
2021-06-06 12:48:57 +0530

    adaskd

--- src/components/HeroBackground/HeroBackground.jsx
+++ src/components/HeroBackground/HeroBackground.jsx
@@ -35,7 +35,7 @@ function HeroBackground() {
                     {text.split('').map((char, i) => {
                       const key = `${i}`;
                       return (
-                        <span key={key} style={i % 2 === 0 ? { color: 'magenta' } : {}}>
+                        <span key={key} style={i % 2 === 0 ? { color: '#0a58ff' } : {}}>
                           {char}
                         </span>
                       );
